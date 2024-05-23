1e4a49418ff11e37f9c0c2bc2bff5ab42c621ec6
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-05-24 02:06:07 +0530

    cc

--- src/section/Hero.tsx
+++ src/section/Hero.tsx
@@ -6,8 +6,16 @@ const Hero = () => {
   return (
     <div className='h-screen flex items-center justify-center relative '>
       <div className='w-full h-full flex items-center justify-center overflow-hidden absolute top-0 '>
-        <video autoPlay loop muted playsInline className='background-video '>
-          <source src='/assets/videos/hero.mp4' type='video/mp4' />
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className='background-video '>
+          <source
+            src='/assets/videos/hero.mp4'
+            type='video/mp4'
+          />
         </video>
         <div className='absolute w-[90%] bottom-[10%] border h-[50%]'>
           <div className='flex items-center justify- justify-between p-4'>
@@ -26,12 +34,11 @@ const Hero = () => {
               <h1 className='text-[5rem] font-medium text-[#bcb8b1] uppercase'>
                 Not for{' '}
                 <span className=' line-through font-light text-red-500'>
-                  Robos
+                  Robots
                 </span>
               </h1>
             </div>
           </div>
-          
         </div>
       </div>
     </div>
