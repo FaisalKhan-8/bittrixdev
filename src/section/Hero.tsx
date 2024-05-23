c45a230866b8f35c1fc8d40c22a0baba08757ff1
Sh-karan27 <shuklakaran107@gmail.com>
2024-05-23 13:11:25 +0530

    Text added in hero

--- src/section/Hero.tsx
+++ src/section/Hero.tsx
@@ -4,6 +4,10 @@ const Hero = () => {
   return (
     <div className='h-screen flex items-center justify-center'>
       Hero section
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quaerat
+        ratione eos necessitatibus!
+      </p>
     </div>
   );
 };
