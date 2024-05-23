b9fadb757c8d065aaf5f01e289f58b037c20fdcb
Sh-karan27 <shuklakaran107@gmail.com>
2024-05-24 01:22:42 +0530

    Hero section added

--- src/section/Hero.tsx
+++ src/section/Hero.tsx
@@ -1,9 +1,39 @@
+import { url } from 'inspector';
 import React from 'react';
+import { HiArrowLongDown } from 'react-icons/hi2';
 
 const Hero = () => {
   return (
-    <div className='h-screen flex items-center justify-center'>
-      Hero section
+    <div className='h-screen flex items-center justify-center relative '>
+      <div className='w-full h-full flex items-center justify-center overflow-hidden absolute top-0 '>
+        <video autoPlay loop muted playsInline className='background-video '>
+          <source src='/assets/videos/hero.mp4' type='video/mp4' />
+        </video>
+        <div className='absolute w-[90%] bottom-[10%] border h-[50%]'>
+          <div className='flex items-center justify- justify-between p-4'>
+            <div className='flex flex-col items-left justify-center'>
+              <h1 className=' text-[6rem] uppercase font-light text-[#bcb8b1]'>
+                WE CREATE Apps
+              </h1>
+              <div className='flex items-center justify-center gap-2'>
+                <div className='bg-white p-5 rounded-full flex items-center justify-center'>
+                  <HiArrowLongDown className=' text-3xl' />
+                </div>
+                <h1 className='text-[6rem]  text-[#bcb8b1] font-medium uppercase'>
+                  Web for human
+                </h1>
+              </div>
+              <h1 className='text-[5rem] font-medium text-[#bcb8b1] uppercase'>
+                Not for{' '}
+                <span className=' line-through font-light text-red-500'>
+                  Robos
+                </span>
+              </h1>
+            </div>
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
 };
--- src/section/Navbar.tsx
+++ src/section/Navbar.tsx
@@ -13,13 +13,13 @@ const sectionLinks = [
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className='w-full grid grid-cols-3 mt-10 bg-primary-color px-4'>
+    <nav className=' w-full flex items-center justify-center mt-10'>
+      <div className='w-[90%] grid grid-cols-3 mt-20  px-4 z-10 absolute'>
         <div className='flex items-center justify-start'>
           <Image src={Logo} alt='logo' className='w-[50px] rounded-full' />
         </div>
 
-        <ul className='flex items-center justify-evenly border rounded-3xl p-2 w-full'>
+        <ul className=' bg-primary-color flex items-center justify-evenly border rounded-3xl p-2 w-full'>
           {sectionLinks.map(({ name, link }, index) => (
             <li key={name} className='text-text-black font-semibold'>
               <Link href={link} className=''>
@@ -27,10 +27,13 @@ const Navbar = () => {
               </Link>
             </li>
           ))}
+          <li className='text-text-black font-semibold'>Contact Us</li>
         </ul>
 
         <div className='flex items-center justify-end gap-2'>
-          <span className='border p-2 rounded-full'>Become a Client</span>
+          <span className='border p-2 rounded-full bg-primary-color text-gray-500'>
+            Become a Client
+          </span>
           <button className='px-4 py-2 border border-secondary-color text-secondary-color rounded-lg'>
             EN
           </button>
