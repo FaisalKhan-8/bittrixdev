74cf39388ebb1d11fff0a3828460e83d15efd1d7
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-05-24 02:23:30 +0530

    color update

--- src/section/Hero.tsx
+++ src/section/Hero.tsx
@@ -20,18 +20,18 @@ const Hero = () => {
         <div className='absolute w-[90%] bottom-[10%] border h-[60%]'>
           <div className='flex items-center justify- justify-between p-4'>
             <div className='flex flex-col items-left justify-center'>
-              <h1 className=' text-[5rem] uppercase font-light text-[#bcb8b1]'>
+              <h1 className=' text-[5rem] uppercase font-light text-primary-color'>
                 WE CREATE Apps
               </h1>
               <div className='flex items-center justify-center gap-2'>
                 <div className='bg-white p-5 rounded-full flex items-center justify-center'>
                   <HiArrowLongDown className=' text-3xl' />
                 </div>
-                <h1 className='text-[6rem]  text-[#bcb8b1] font-medium uppercase'>
+                <h1 className='text-[6rem]   text-primary-color font-medium uppercase'>
                   Web for human
                 </h1>
               </div>
-              <h1 className='text-[5rem] font-medium text-[#bcb8b1] uppercase'>
+              <h1 className='text-[5rem] font-medium  text-primary-color uppercase'>
                 Not for{' '}
                 <span className=' line-through font-light text-red-500'>
                   Robots
