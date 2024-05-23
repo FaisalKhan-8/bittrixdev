b9345cb7bbd48bba57ac1affd11aafa2cf52ec8b
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-05-23 13:27:00 +0530

    cc

--- src/section/Hero.tsx
+++ src/section/Hero.tsx
@@ -4,12 +4,6 @@ const Hero = () => {
   return (
     <div className='h-screen flex items-center justify-center'>
       Hero section
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quaerat
-        ratione eos necessitatibus! hello how are you
-      </p>
-      <p>this is para 2</p>
-      <p>Hey there</p>
     </div>
   );
 };
