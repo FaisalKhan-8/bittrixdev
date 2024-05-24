ed92bd3e273a3f4dcd004448072da1cda8adbd21
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-05-25 02:01:52 +0530

    cc

--- src/section/Service.tsx
+++ src/section/Service.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Service = () => {
   return (
-    <div className='h-screen flex items-center justify-center'>Service</div>
+    <div
+      id='services'
+      className='h-screen flex items-center justify-center'>
+      Service
+    </div>
   );
 };
 
