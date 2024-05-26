d47dc004d6aff31a3b76197a556bd76efa692ba5
FaisalKhan-8 <mdfaisalkhan1500@gmail.com>
2024-05-26 09:51:47 +0530

    cc

--- src/section/Service.tsx
+++ src/section/Service.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import {services} from '../../public/data/data';
+import { services } from '../../public/data/data';
 
 const Service = () => {
   return (
-    <div id='services' className='h-screen flex items-center justify-center'>
+    <div
+      id='services'
+      className='h-screen flex items-center justify-center'>
       <div className='w-full h-full flex flex-col items-center justify-start gap-10 overflow-hidden p-10'>
         <div className=' flex flex-col items-left justify-center gap-5'>
           <h3 className='text-xl'>01-Our Services</h3>
@@ -14,9 +16,19 @@ const Service = () => {
         </div>
         <div className='w-full flex  items-left justify-center gap-5'>
           {services.map((curr) => (
-            <div className='serviceItem flex flex-col items-left justify-end gap-5 p-4'>
-              <video autoPlay loop muted playsInline className='serviceVideo'>
-                <source src={curr.video} type='video/mp4' />
+            <div
+              key={curr.title}
+              className='serviceItem flex flex-col items-left justify-end gap-5 p-4'>
+              <video
+                autoPlay
+                loop
+                muted
+                playsInline
+                className='serviceVideo'>
+                <source
+                  src={curr.video}
+                  type='video/mp4'
+                />
               </video>
               <h1 className=' text-primary-color text-3xl uppercase font-bold'>
                 {curr.title}
