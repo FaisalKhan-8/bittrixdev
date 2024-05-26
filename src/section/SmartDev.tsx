b6796907eeaa865f8b665d02163c26cbb895b31b
Sh-karan27 <shuklakaran107@gmail.com>
2024-05-26 18:51:20 +0530

    added smartDev sectionn

--- src/section/SmartDev.tsx
+++ src/section/SmartDev.tsx
@@ -1,8 +1,43 @@
 import React from 'react';
+import phone1 from '../../public/assets/phone-01.webp';
+import phone2 from '../../public/assets/phone-02.webp';
+import Image from 'next/image';
 
 const SmartDev = () => {
   return (
-    <div className='h-screen flex items-center justify-center'>SmartDev</div>
+    <div className='h-screen w-full flex items-center justify-center'>
+      <div className='w-full h-full flex flex-col items-start justify-between p-10 relative'>
+        <div className='flex flex-col items-left justify-center'>
+          <h1 className=' text-text-color text-8xl'>{'{' + 'SMART' + '}'}</h1>
+          <h1 className='text-text-color text-8xl'>DEVELOPMENT</h1>
+        </div>
+        <div className=' w-full flex items-center justify-center'>
+          <h1 className=' text-text-color text-9xl uppercase'>
+            back ground text
+          </h1>
+        </div>
+        <div className='w-full flex items-center justify-between'>
+          <p className='w-1/4 text-wrap text-text-color text-xl'>
+            Making your business outstanding is a science. We take it (a)
+            seriously and (b) creatively.
+          </p>
+          <div className='flex items-center justify-center gap-5'>
+            <span className=' uppercase text-xl'>Check our portfolio</span>
+            <span className=' uppercase text-xl text-text-color'>
+              Get In Touch
+            </span>
+          </div>
+        </div>
+        <div className='flex items-center  justify-center absolute w-full top-[30%] '>
+          <Image src={phone1} alt='phonn1' className='w-[300px] ' />
+          <Image
+            src={phone2}
+            alt='phonn1'
+            className='w-[300px] absolute left-[50%] top-[33%] '
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
