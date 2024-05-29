a8aac7ef9103a87c0fbc514419502d3308dab814
Sh-karan27 <shuklakaran107@gmail.com>
2024-05-29 11:18:09 +0530

    change in SmatDev section

--- src/section/HoldingPhone.tsx
+++ src/section/HoldingPhone.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
-
+import phoneHold from '../../public/assets/holdingphone.png';
+import Image from 'next/image';
+import { Vortex } from '@/components/ui/vortex';
+import {
+  TextRevealCard,
+  TextRevealCardDescription,
+  TextRevealCardTitle,
+} from '../components/ui/text-reveal-card';
 const HoldingPhone = () => {
   return (
-    <div className='h-screen flex items-center justify-center'>
-      HoldingPhone
+    <div className='h-screen w-full flex items-center justify-center overflow-hidden'>
+      <Vortex
+        backgroundColor='black'
+        className=' w-full  h-full flex items-center justify-center'>
+        <TextRevealCard
+          text='You know the business'
+          revealText='I know the chemistry '>
+          <TextRevealCardTitle>
+            Sometimes, you just need to see it.
+          </TextRevealCardTitle>
+          <TextRevealCardDescription>
+            This is a text reveal card. Hover over the card to reveal the hidden
+            text.
+          </TextRevealCardDescription>
+        </TextRevealCard>
+      </Vortex>
     </div>
   );
 };
--- src/section/SmartDev.tsx
+++ src/section/SmartDev.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 
 const SmartDev = () => {
   return (
-    <div className='h-screen w-full flex items-center justify-center'>
+    <div className='h-screen w-full flex items-center justify-center bg-black'>
       <div className='w-full h-full flex flex-col items-start justify-between p-10 relative'>
         <div className='flex flex-col items-left justify-center'>
           <h1 className=' text-text-color text-8xl'>{'{' + 'SMART' + '}'}</h1>
@@ -22,7 +22,9 @@ const SmartDev = () => {
             seriously and (b) creatively.
           </p>
           <div className='flex items-center justify-center gap-5'>
-            <span className=' uppercase text-xl'>Check our portfolio</span>
+            <span className=' uppercase text-xl text-text-color'>
+              Check our portfolio
+            </span>
             <span className=' uppercase text-xl text-text-color'>
               Get In Touch
             </span>
@@ -33,7 +35,7 @@ const SmartDev = () => {
           <Image
             src={phone2}
             alt='phonn1'
-            className='w-[300px] absolute left-[50%] top-[33%] '
+            className='w-[300px] absolute left-[50%] top-[30%] '
           />
         </div>
       </div>
